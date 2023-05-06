5fcd4a472b1760a1b929bb83f5c3017017558380
Braxton Brown <braxton.brown@outlook.com>
2023-05-05 18:59:48 -0600

    initial commit

---
+++ Client.py
@@ -0,0 +1,104 @@
+import discord
+import os
+import re
+import torch
+import Model as m
+
+if os.path.exists('token.txt'):
+    with open('token.txt', 'r') as f:
+        token = f.read()
+else:
+    token = input('Enter your token:\n')
+    cache_token = input('Cache token for future use? (y/n)\n').lower()
+    if cache_token == 'y':
+        with open('token.txt', 'w') as f:
+            f.write(token)
+
+help_text = """Commands:
+    n - refers to the "n" most recent messages used for context in the channel.
+    n2 - refers to the "n2" most recent messages to skip when using context in the channel.
+    <> - required argument
+    [] - optional argument
+    ... - any number of arguments
+    user_name - @mention of the user
+
+    `help` - display this message
+    `summarize <n> [n2]` - summarize the last n messages, optionally skipping the last n2 messages
+    `query <n> [n2] <...>` - query the chatbot with the given text, optionally skipping the last n2 messages. Ex: `query 10 2 What conclusions can we draw from this?`
+    `prompt <...>` - prompt the bare chatbot with the given text
+    `act_like <user_name> <n> [n2]` - act like the user with the given name and respond as them. n is the number of messages for context, optionally skipping the last n2 messages
+    """
+commands = [r'(?P<command>help)',
+            r'(?P<command>summarize)\s+(?P<n>\d+)(?:\s+(?P<n2>\d*))?',
+            r'(?P<command>query)\s+(?P<n>\d+)(?:\s+(?P<n2>\d+))?\s+(?P<text>.+)',
+            r'(?P<command>prompt)\s+(?P<text>.+)',
+            r'(?P<command>act_like)\s+(?P<user>[^\s]+)\s+(?P<n>\d+)(?:\s+(?P<n2>\d*))?',]
+
+class myClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user} ({self.user.mention})')
+
+    async def format_messages(self, content, message, n, n2='0'):
+        if not n.isdigit():
+            return
+        n2 = int(n2) if isinstance(n2, str) and n2.isdigit() else 0
+        sent_message = await message.channel.send('Working on it...')
+        sent_message_content = ''
+        messages = message.channel.history(limit=int(n)+1)
+        messages = (await messages.flatten())[:n2:-1]
+        for message in messages:
+            content = message.content
+            for user in message.mentions:
+                content = re.sub(f'<@!?{user.id}>', user.name, content)
+            
+            sent_message_content += f'{str(message.author)[:-5]}: {content}\n\n'
+        return sent_message, sent_message_content
+
+    async def send_message(self, generator, sent_message):
+        for i, response in enumerate(generator):
+            if response and i != 0 and i % 6 == 0 and not self.is_ws_ratelimited():
+                await sent_message.edit(content=response)
+            print(response, end='\r\r')
+        return await sent_message.edit(content=response)
+
+    async def on_message(self, message):
+        torch.cuda.empty_cache()
+        if not message.content:
+            return
+        content = message.content.split()
+        mentions = {user.id: user.name for user in  message.mentions}
+        if content.pop(0) != self.user.mention:
+            return
+        if not content:
+            return
+        content = ' '.join(content)
+
+        mat = self.get_matching_command(content)
+        command = mat.group('command')
+
+        if command == 'help':
+            await message.channel.send(help_text)
+            return
+        if command == 'summarize':
+            sent_message, sent_message_content = await self.format_messages(content, message, mat.group('n'), mat.group('n2'))
+            return await self.send_message(m.summarize(sent_message_content, 2048), sent_message)
+        if command == 'query':
+            sent_message, sent_message_content = await self.format_messages(content, message, mat.group('n'), mat.group('n2'))
+            return await self.send_message(m.query(sent_message_content, mat.group('text'), 2048), sent_message)
+        if command == 'prompt':
+            sent_message = await message.channel.send('Working on it...')
+            return await self.send_message(m.prompt(mat.group('text'), 2048), sent_message)
+        if command == 'act_like':
+            sent_message, sent_message_content = await self.format_messages(content, message, mat.group('n'), mat.group('n2'))
+            username = re.sub(r'<@!?(\d+)>', r'\1', mat.group('user'))
+            return await self.send_message(m.act_like(sent_message_content, username, 2048), sent_message)
+
+    def get_matching_command(self, content):
+        for command in commands:
+            if mat:=re.match(command, content):
+                return mat
+        return None
+
+if __name__ == '__main__':
+    client = myClient()
+    client.run(token, bot=False)
---
+++ Model.py
@@ -0,0 +1,87 @@
+from transformers import AutoModel, AutoTokenizer
+import markdownify
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().quantize(8).cuda()
+model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True, device_map='auto').half()
+
+
+model = model.eval()
+
+max_length = 256
+top_p = 0.7
+temperature = 0.95
+
+
+def parse_text(text):
+    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
+    lines = text.split("\n")
+    lines = [line for line in lines if line != ""]
+    count = 0
+    for i, line in enumerate(lines):
+        if "```" in line:
+            count += 1
+            items = line.split('`')
+            if count % 2 == 1:
+                lines[i] = f'<pre><code class="language-{items[-1]}">'
+            else:
+                lines[i] = f'<br></code></pre>'
+        else:
+            if i > 0:
+                if count % 2 == 1:
+                    line = line.replace("`", "\`")
+                    line = line.replace("<", "&lt;")
+                    line = line.replace(">", "&gt;")
+                    line = line.replace(" ", "&nbsp;")
+                    line = line.replace("*", "&ast;")
+                    line = line.replace("_", "&lowbar;")
+                    line = line.replace("-", "&#45;")
+                    line = line.replace(".", "&#46;")
+                    line = line.replace("!", "&#33;")
+                    line = line.replace("(", "&#40;")
+                    line = line.replace(")", "&#41;")
+                    line = line.replace("$", "&#36;")
+                lines[i] = "<br>"+line
+    text = "".join(lines)
+    return text
+
+
+def predict(input, max_length, top_p, temperature):
+    torch.cuda.empty_cache()
+    input = parse_text(input)
+    tokenized_input = tokenizer.encode(input, return_tensors="pt")
+    if tokenized_input.shape[1] > max_length:
+        return f"Input is too long. It has {tokenized_input.shape[1]} tokens, but the maximum is {max_length} tokens. Please shorten the input and try again."
+    for response, history in model.stream_chat(tokenizer, input, None, max_length=max_length, top_p=top_p,
+                                               temperature=temperature):
+        yield markdownify.markdownify(response, heading_style="ATX", bullets="-")
+
+def summarize(input, max_length):
+    input = 'Pretend you are an AI that summarizes a conversation. Summarize the following conversation:\n\n' + input + '\n\nPlease summarize the conversation in as few words as possible.'
+    for response in predict(input, max_length, top_p, temperature):
+        yield response
+    
+def query(input, query, max_length):
+    input = f'Answer the following question "{query}" based on the following conversation:\n\n{input}\n\nPlease answer the question "{query}" in as few words as possible.'
+    for response in predict(input, max_length, top_p, temperature):
+        yield response
+    
+def prompt(input, max_length):
+    for response in predict(input, max_length, top_p, temperature):
+        yield response
+    
+def act_like(input, person, max_length):
+    input = f'Pretend you are "{person}". Act like "{person}" and respond to the following conversation:\n\n{input}\n\nPlease respond how {person} would respond. Do not respond for anyone else.'
+    for response in predict(input, max_length, top_p, temperature):
+        yield response
+
+if __name__ == "__main__":
+    for words in summarize("""
+ShortGuy#3808: summarize the 5 articles
+YourMom#3345: Working on it...
+ShortGuy#3808: ShortGuy summarize in 
+ShortGuy#3808: ShortGuy#3808: ShortGuy summarize 5
+ShortGuy#3808: Working on it...
+ShortGuy#3808: ShortGuy summarize 3""", 512):
+        print(words, end='\r\r')
