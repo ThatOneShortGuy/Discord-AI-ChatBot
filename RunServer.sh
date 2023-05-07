ee5118ea08341857f2de90c8ec21c632bbd7b155
Braxton Brown <braxton.brown@outlook.com>
2023-05-06 21:01:37 -0600

    Update Requrirements

--- RunServer.sh
+++ RunServer.sh
@@ -3,5 +3,9 @@ wget https://repo.radeon.com/amdgpu-install/5.4/ubuntu/jammy/amdgpu-install_5.4.
 sudo apt-get install ./amdgpu-install_5.4.50400-1_all.deb
 
 sudo amdgpu-install --usecase=rocm
+sudo apt install python3-pip
 
 cd Discord-AI-Chatbot
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.4.2
+pip install -r requirements.txt
