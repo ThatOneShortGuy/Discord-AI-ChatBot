53a8b75158bb1730ebdb0c486b67a33f2a56a2da
Braxton Brown <braxton.brown@outlook.com>
2023-05-18 12:01:34 -0600

    remove IP_ADDR

--- HostInferenceServer.py
+++ HostInferenceServer.py
@@ -10,7 +10,6 @@ import re
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
-IP_ADDR = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 app = Flask(__name__)
 
 MODEL_NAME = "OpenAssistant/pythia-12b-sft-v8-7k-steps"
